2e1c4dd856cf8e4c50626d3319c125b5eb25371c
Jacek <jacek.jankowiak@accenture.com>
2023-11-13 13:43:42 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Reinforcement learning agents showcase')
+st.markdown('</br></br></br>', unsafe_allow_html=True)
+
+col1, _, col2 = st.columns([4, 1, 4])
+
+with col1:
+    st.image('images/pond/0.jpg')
+
+
+with col2:
+    st.image('images/pond/0.jpg')
